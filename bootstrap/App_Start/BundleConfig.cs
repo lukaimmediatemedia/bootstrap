6d9f659c22b2080c8e0aa5540624601b39ebf6bf
Luka <lukasz.sobiech@immediate.co.uk>
2014-03-19 00:10:35 +0000

    I have added some extra stuff

--- bootstrap/App_Start/BundleConfig.cs
+++ bootstrap/App_Start/BundleConfig.cs
@@ -16,6 +16,23 @@ namespace bootstrap
             bsSassCss.Include("~/Content/css/sass/bootstrap.css");
             bundles.Add(bsSassCss);
 
+            //SASS JS
+            var bsSassJs = new ScriptBundle("~/bundles/sass-js");
+            bsSassJs.Include(
+                "~/Scripts/js/sass/bootstrap/affix.js",
+                "~/Scripts/js/sass/bootstrap/alert.js",
+                "~/Scripts/js/sass/bootstrap/button.js",
+                "~/Scripts/js/sass/bootstrap/carousel.js",
+                "~/Scripts/js/sass/bootstrap/collapse.js",
+                "~/Scripts/js/sass/bootstrap/dropdown.js",
+                "~/Scripts/js/sass/bootstrap/tab.js",
+                "~/Scripts/js/sass/bootstrap/transition.js",
+                "~/Scripts/js/sass/bootstrap/scrollspy.js",
+                "~/Scripts/js/sass/bootstrap/modal.js",
+                "~/Scripts/js/sass/bootstrap/tooltip.js",
+                "~/Scripts/js/sass/bootstrap/popover.js");
+            bundles.Add(bsSassJs);
+
             //LESS CSS
             var bsLessCss = new StyleBundle("~/bundles/less");
             bsLessCss.Include("~/Content/css/less/bootstrap.css");
